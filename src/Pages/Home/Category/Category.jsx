614161ec63b159299f782c1d8c59c50b9bbf825f
ShahJamal6160 <shahj6160@gmail.com>
2025-02-22 21:00:28 +0600

    edit slider section

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -19,22 +19,27 @@ const Category = () => {
           clickable: true,
         }}
         modules={[Pagination]}
-        className="mySwiper"
+        className="mySwiper mb-24"
       >
         <SwiperSlide>
             <img src={slide1} alt="" />
+            <h3 className='text-4xl uppercase text-center -mt-16 text-white'>Salad</h3>
         </SwiperSlide>
         <SwiperSlide>
             <img src={slide2} alt="" />
+            <h3 className='text-4xl uppercase text-center -mt-16 text-white'>Pizza</h3>
         </SwiperSlide>
         <SwiperSlide>
             <img src={slide3} alt="" />
+            <h3 className='text-4xl uppercase text-center -mt-16 text-white'>Soup</h3>
         </SwiperSlide>
         <SwiperSlide>
             <img src={slide4} alt="" />
+            <h3 className='text-4xl uppercase text-center -mt-16 text-white'>Dessert</h3>
         </SwiperSlide>
         <SwiperSlide>
             <img src={slide5} alt="" />
+            <h3 className='text-4xl uppercase text-center -mt-16 text-white'>Salad</h3>
         </SwiperSlide>
      
       </Swiper>
